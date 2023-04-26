8d14fe55c707efc874848e87641282a7211b4839
João Henrique <joaohnsousa@gmail.com>
2023-04-26 14:56:52 -0300

    Completo

--- src/components/Cards/CardDetails.js
+++ src/components/Cards/CardDetails.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-
+import Navbar from "../Navbar/Navbar";
 
 const CardDetails = () => {
   let { id } = useParams();
@@ -16,44 +16,45 @@ const CardDetails = () => {
     })();
   }, [api]);
 
-  let { name, gender, image, location, origin, species, status} =
-    fetchData;
+  let { name, gender, image, location, origin, species, status } = fetchData;
 
   return (
-    
-    <div className="container d-flex justify-content-center">
-      <div className="d-flex flex-column gap-3 my-5">
-        
-        <img className="img-fluid" src={image} alt="personagem"></img>
-        {(() => {
-          if (status === "Dead") {
-            return <div className={`badge bg-danger fs-5`}>{status}</div>;
-          } else if (status === "Alive") {
-            return <div className={`badge bg-success fs-5`}>{status}</div>;
-          } else {
-            return <div className={`badge bg-secondary fs-5`}>{status}</div>;
-          }
-        })()}
-        <div className="Container">
-        <div className="">
-            <span className="fw-bold">Nome: </span>{name}
-          </div>
-        
-          <div className="">
-            <span className="fw-bold">Gênero:</span> {gender}
-          </div>
-          <div className="">
-            <span className="fw-bold">Localização:</span> {location?.name}
-          </div>
-          <div className="">
-            <span className="fw-bold">Espécie:</span> {species}
-          </div>
-          <div className="">
-            <span className="fw-bold">Origem:</span> {origin?.name}
+    <>
+      <Navbar />
+      <div className="container d-flex justify-content-center">
+        <div className="d-flex flex-column gap-3 my-5">
+          <img className="img-fluid" src={image} alt="personagem"></img>
+          {(() => {
+            if (status === "Dead") {
+              return <div className={`badge bg-danger fs-5`}>{status}</div>;
+            } else if (status === "Alive") {
+              return <div className={`badge bg-success fs-5`}>{status}</div>;
+            } else {
+              return <div className={`badge bg-secondary fs-5`}>{status}</div>;
+            }
+          })()}
+          <div className="Container">
+            <div className="">
+              <span className="fw-bold">Nome: </span>
+              {name}
+            </div>
+
+            <div className="">
+              <span className="fw-bold">Gênero:</span> {gender}
+            </div>
+            <div className="">
+              <span className="fw-bold">Localização:</span> {location?.name}
+            </div>
+            <div className="">
+              <span className="fw-bold">Espécie:</span> {species}
+            </div>
+            <div className="">
+              <span className="fw-bold">Origem:</span> {origin?.name}
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
